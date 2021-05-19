a8f004533b5becdf34460ef86caf91180b3a7962
PLara-r <tl1226932@gmail.com>
2021-05-19 14:39:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main  extends Canine {
+    public double getAverageWeight() {
+        return super.getAverageWeight()+20;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Canine().getAverageWeight());
+        System.out.println(new Main().getAverageWeight());
+    }}
+
